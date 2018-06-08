600b020ab0ac7f1faf02da17abf1ecbc79283f63
Med Wasfy <medwasfy@gmail.com>
2018-06-08 09:52:14 -0500

    animte bug sorted

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+$( document ).ready(function() {
+
+    // create An array of static action to show on the page load
+    // new search will be pushed and added to this array later on
+    var gifArr = ["metallica", "simpsons", "family guy", "ufc", "freestyle", "speedracer", "Sky Diving", "Skiing", "Hiking","Beach", "Laughing", "snooping"];
+    
+    // Function that displays all gif buttons
+    function allBtns(){
+
+        // empty div to allow for new content
+        $("#gifsBtns").empty(); 
+        // create a for loop to go through the gifs
+        for (var i = 0; i < gifArr.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("search");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", gifArr[i]);
+            gifButton.text(gifArr[i]);
+            $("#gifsBtns").append(gifButton);
+        }
+    }
+    // Function to dynamically add a new giphy search button to the gifArr array
+    function addNewBtn(){
+        
+        $("#addGif").on("click", function(){
+        var search = $("#search-input").val().trim();
+    // prevent user from adding blank buttons
+        if (search == ""){
+          return false; 
+        }
+        gifArr.push(search);
+    
+        allBtns();
+        return false;
+        });
+    }
+    // remove last search button
+    function rmLastBtn(){
+        $("removeGif").on("click", function(){
+        gifArr.pop(search);
+        allBtns();
+        return false;
+        });
+    }
+
+
+    // create an ajax call to retreive all the gif buttons
+    // assign it a for loop to display all the buttons
+    // wrap it all in a callback function 
+    // wil need to use this since the active element is dynamically changed 
+    function displayGifs(){
+
+        var action = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=EqafGIOM5YVRLUuvprPivbXuOd4EOUs1&limit=10";
+        console.log(queryURL); 
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            console.log(response); 
+    
+            // clear gifsView div everytime the button click is triggred to make space for new content
+            $("#gifsView").empty();
+    
+            // return results into a variable called results
+            var results = response.data; 
+            if (results == ""){
+              console.log("NO content for this search, Try Again!");
+            }
+            for (var i=0; i<results.length; i++){
+
+                // create dynamic divs to hold the results from the search 
+                var gifDiv = $("<div>"); 
+                gifDiv.addClass("gifDiv");
+
+                // get rating of gif in the result var
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                // display rating on the page
+                gifDiv.append(gifRating);
+
+                // assign image tag to the gif pulled
+                var gifImage = $("<img>");
+
+                // pull the gifs from the source 
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+
+                // still gifs
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
+
+
+                // animated gifs
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+
+                // set the default state of the image
+                gifImage.attr("data-state", "still"); 
+
+                // add a class of image for css
+                gifImage.addClass("image");
+
+                //disply the gif on the page
+                gifDiv.append(gifImage);
+                
+                // adding div of gifs to display on the page
+                $("#gifsView").prepend(gifDiv);
+            }
+        });
+    }
+    
+    allBtns(); 
+    addNewBtn();
+    rmLastBtn();
+
+    // add eventlistner on image click to either animate or still
+    $(document).on("click", ".search", displayGifs);
+
+    $(document).on("click", ".image", function(){
+
+        var state = $(this).attr('data-state');
+        if ( state === 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+    });
+    
